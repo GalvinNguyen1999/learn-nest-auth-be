d230970cbf78c38c64af8123f5eefcc342c3dde7
Cuong Nguyen <galvinnguyen1999@gmail.com>
2025-06-29 12:14:54 +0700

    feat: save user in database

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,10 +1,5 @@
-import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
-
 export class CreateUserDto {
-  @IsEmail()
   email: string;
-
-  @MinLength(6)
-  @IsNotEmpty()
-  password: string;
+  name?: string;
+  avatar?: string;
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,13 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UserService {
   constructor(private readonly prisma: PrismaService) {}
 
-  async create(data: { email: string; password: string }) {
-    return this.prisma.user.create({
-      data,
+  async create(data: CreateUserDto) {
+    return this.prisma.user.upsert({
+      where: { email: data.email },
+      update: { name: data.name, avatar: data.avatar },
+      create: {
+        email: data.email,
+        name: data.name,
+        avatar: data.avatar,
+      },
     });
   }
 }
